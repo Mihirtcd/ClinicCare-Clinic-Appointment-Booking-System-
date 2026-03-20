fbd192136051d93e22bab97c0164fd2d73d85c3b
Mihir Shekhar <a00335637@student.tus.ie>
2026-03-20 03:17:59 +0000

    build(swagger): add springdoc openapi dependency and base config

---
+++ cliniccare/src/main/java/com/tus/cliniccare/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.tus.cliniccare.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI clinicCareOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ClinicCare REST API")
+                        .version("v1")
+                        .description("ClinicCare backend APIs for patient, doctor, and admin workflows."))
+                .components(new Components().addSecuritySchemes(
+                        "bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ));
+    }
+}
